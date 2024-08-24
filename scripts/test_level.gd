462ad298560db70404ebe07b1756913acd6d2a66
HeyNeek <nikoaguirre9315@gmail.com>
2024-08-24 09:43:15 -0700

    added vertical animation, needs more tweaking on gravity

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = $Player
-@onready var movingplatform1: AnimationPlayer = $MovingPlatform/AnimationPlayer
+@onready var moving_platform1: AnimationPlayer = $MovingPlatform/AnimationPlayer
+@onready var moving_platform2: AnimationPlayer = $MovingPlatform2/AnimationPlayer
 
 func _ready() -> void:
 	#Engine.max_fps = 60
-	movingplatform1.play("move_left_to_right")
+	moving_platform1.play("move_left_to_right")
+	await get_tree().create_timer(2.5).timeout
+	moving_platform2.play("move_up_to_down")
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reset_level"):
